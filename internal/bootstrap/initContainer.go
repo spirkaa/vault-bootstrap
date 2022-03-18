3136ca88fc273116e9775d1d8bdc9f710ea6dfd5
Ilya Pavlov <piv@devmem.ru>
2022-03-18 19:02:51 +0300

    updated init container job spec

--- internal/bootstrap/initContainer.go
+++ internal/bootstrap/initContainer.go
@@ -15,9 +15,6 @@ import (
 	"k8s.io/client-go/rest"
 )
 
-var vaultInitContainerImage string
-var vaultJobImage string
-
 func InitContainer() {
 	// Create clientSet for k8s client-go
 	k8sConfig, err := rest.InClusterConfig()
@@ -32,39 +29,21 @@ func InitContainer() {
 		os.Exit(1)
 	}
 
-	// Define Parameters
 	podName, ok := os.LookupEnv("VAULT_K8S_POD_NAME")
 	if !ok {
 		panic("Cannot extract Pod name from environment variables")
 	}
+
 	pod, err := clientsetK8s.CoreV1().Pods(namespace).Get(context.TODO(), podName, metav1.GetOptions{})
 	if err != nil {
 		log.Error(err.Error())
 		panic("Cannot extract Pod information from Kubernetes API")
 	}
-	// eventually parametrize http scheme and port
-	podUrl := "https://" + podName + "." + pod.Spec.Subdomain + ":8200"
-	log.Infof("Pod URL: %s", podUrl)
-
-	vaultInitContainerImage = pod.Status.InitContainerStatuses[0].Image
-	// Get Vault image from env variables (if defined)
-	if extrVaultJobImage, ok := os.LookupEnv("VAULT_JOB_IMAGE"); !ok {
-		log.Warn("VAULT_JOB_IMAGE is not set. Defaulting to ", vaultInitContainerImage)
-		vaultJobImage = vaultInitContainerImage
-	} else {
-		vaultJobImage = extrVaultJobImage
-	}
-
-	namespace, ok := os.LookupEnv("VAULT_K8S_NAMESPACE")
-	if !ok {
-		panic("Cannot extract Namespace name from environment variables")
-	}
 
 	randomString := strings.Replace(uuid.New().String(), "-", "", -1)
-	jobName := podName + "-usealer-" + randomString[0:4]
+	jobName := podName + "-bootstrap-" + randomString[0:4]
+	JobImage := pod.Status.InitContainerStatuses[0].Image
 
-	// Define Job
-	jobClient := clientsetK8s.BatchV1().Jobs(namespace)
 	job := &batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      jobName,
@@ -73,32 +52,20 @@ func InitContainer() {
 		Spec: batchv1.JobSpec{
 			Template: corev1.PodTemplateSpec{
 				Spec: corev1.PodSpec{
-					RestartPolicy:      "OnFailure",
+					RestartPolicy:      "Never",
 					ServiceAccountName: vaultServiceAccount,
 					Containers: []corev1.Container{
 						{
-							Name:  "unsealer",
-							Image: vaultJobImage,
+							Name:  jobName,
+							Image: JobImage,
 							Env: []corev1.EnvVar{
 								{
-									Name:  "VAULT_ENABLE_INIT",
-									Value: "False",
-								},
-								{
-									Name:  "VAULT_ENABLE_K8SSECRET",
-									Value: "False",
-								},
-								{
-									Name:  "VAULT_ENABLE_UNSEAL",
-									Value: "True",
-								},
-								{
-									Name:  "VAULT_ENABLE_K8SAUTH",
-									Value: "False",
+									Name:  "VAULT_ADDR",
+									Value: vaultAddr,
 								},
 								{
 									Name:  "VAULT_CLUSTER_MEMBERS",
-									Value: podUrl,
+									Value: vaultClusterMembers,
 								},
 								{
 									Name:  "VAULT_KEY_SHARES",
@@ -108,6 +75,22 @@ func InitContainer() {
 									Name:  "VAULT_KEY_THRESHOLD",
 									Value: strconv.Itoa(vaultKeyThreshold),
 								},
+								{
+									Name:  "VAULT_ENABLE_INIT",
+									Value: strconv.FormatBool(vaultInit),
+								},
+								{
+									Name:  "VAULT_ENABLE_K8SSECRET",
+									Value: strconv.FormatBool(vaultK8sSecret),
+								},
+								{
+									Name:  "VAULT_ENABLE_UNSEAL",
+									Value: strconv.FormatBool(vaultUnseal),
+								},
+								{
+									Name:  "VAULT_ENABLE_K8SAUTH",
+									Value: strconv.FormatBool(vaultK8sAuth),
+								},
 							},
 						},
 					},
@@ -116,10 +99,10 @@ func InitContainer() {
 		},
 	}
 
-	result, err := jobClient.Create(context.TODO(), job, metav1.CreateOptions{})
+	result, err := clientsetK8s.BatchV1().Jobs(namespace).Create(context.TODO(), job, metav1.CreateOptions{})
 	if err != nil {
 		log.Error(err.Error())
-		panic("Failed to create Vault Unseal Job")
+		panic("Failed to create job")
 	}
-	log.Info("Created Vault Unseal Job ", result.GetObjectMeta().GetName())
+	log.Info("Created job ", result.GetObjectMeta().GetName())
 }
